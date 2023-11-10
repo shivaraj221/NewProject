2100cb59cd400a42ace6230e1c38cf8c9cf74aeb
shivaraj221 <139696646+shivaraj221@users.noreply.github.com>
2023-11-10 23:43:29 +0530

    Update project3.java

--- project3.java
+++ project3.java
@@ -1,20 +1,76 @@
 import java.util.Scanner;
-public class Solution{
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter the coefficient of x^2: ");
-        double a = input.nextDouble();
-        System.out.print("Enter the coefficient of x: ");
-        double b = input.nextDouble();
-        System.out.print("Enter the constant term: ");
-        double c = input.nextDouble();
-        double discriminant = b * b - 4 * a * c;
-        if (discriminant >= 0) {
-            double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-            double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-            System.out.println("The roots are: " + root1 + ", " + root2);
+
+class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        balance = initialBalance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: $" + amount);
         } else {
-            System.out.println("The roots are complex.");
+            System.out.println("Invalid amount for deposit");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && balance >= amount) {
+            balance -= amount;
+            System.out.println("Withdrawn: $" + amount);
+        } else if (amount <= 0) {
+            System.out.println("Invalid amount for withdrawal");
+        } else {
+            System.out.println("Insufficient balance");
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
+
+public class ATMSystem {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter initial balance: $");
+        double initialBalance = scanner.nextDouble();
+
+        BankAccount account = new BankAccount(initialBalance);
+
+        while (true) {
+            System.out.println("\nATM Menu:");
+            System.out.println("1. Deposit");
+            System.out.println("2. Withdraw");
+            System.out.println("3. Check Balance");
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice: ");
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter deposit amount: $");
+                    double depositAmount = scanner.nextDouble();
+                    account.deposit(depositAmount);
+                    break;
+                case 2:
+                    System.out.print("Enter withdrawal amount: $");
+                    double withdrawalAmount = scanner.nextDouble();
+                    account.withdraw(withdrawalAmount);
+                    break;
+                case 3:
+                    System.out.println("Current Balance: $" + account.getBalance());
+                    break;
+                case 4:
+                    System.out.println("Exiting ATM. Have a nice day!");
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice. Please select a valid option.");
+            }
         }
     }
 }
